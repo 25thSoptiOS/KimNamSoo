8665d7587e351bdb312eea3ad28c8ebdf1738115
namsoo5 <are34@naver.com>
2019-10-15 12:39:34 +0900

    뷰클릭시 키보드 내리기

--- SOPT_2thTraining/SOPT_2thTraining/Sources/ViewController/ViewController.swift
+++ SOPT_2thTraining/SOPT_2thTraining/Sources/ViewController/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var idtextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loginButtonSet()
-        
+        initGestureRecognizer()
     }
     
     // MARK: 로그인버튼 테두리
@@ -28,3 +30,116 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIGestureRecognizerDelegate{
+    
+    //MARK: - 키보드 내리기
+    //TextField를 터치했는지 판별
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if touch.view?.isDescendant(of: idtextField) ?? false || touch.view?.isDescendant(of: pwTextField) ?? false {
+            //터치된 뷰가 TextField면 터치를 안한것처럼 이벤트를 주지않고 키보드가 꺼지지 않도록
+            return false
+        }
+        
+        return true
+    }
+    
+    //GestureRecognizer 생성
+    func initGestureRecognizer() {
+        let textFieldTap = UITapGestureRecognizer(target: self, action: #selector(handleTapTextField(_:)))
+        textFieldTap.delegate = self
+        view.addGestureRecognizer(textFieldTap)
+    }
+    
+    // 다른 위치 탭했을 때 키보드 없어지는 코드
+       @objc func handleTapTextField(_ sender: UITapGestureRecognizer) {
+           self.idtextField.resignFirstResponder()
+           self.pwTextField.resignFirstResponder()
+       }
+       
+    //MARK: - 키보드에 따른 뷰의 이동
+    
+}
+
+/*
+// 키보드 때문에 가려지지 않게 조정하는 extension
+extension ViewController: UIGestureRecognizerDelegate {
+    
+    func initGestureRecognizer() {
+        let textFieldTap = UITapGestureRecognizer(target: self, action: #selector(handleTapTextField(_:)))
+        textFieldTap.delegate = self
+        view.addGestureRecognizer(textFieldTap)
+    }
+    
+    // 다른 위치 탭했을 때 키보드 없어지는 코드
+    @objc func handleTapTextField(_ sender: UITapGestureRecognizer) {
+        self.loginUserid.resignFirstResponder()
+        self.loginPassword.resignFirstResponder()
+    }
+    
+    
+    func gestureRecognizer(_ gestrueRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if (touch.view?.isDescendant(of: loginUserid))! || (touch.view?.isDescendant(of: loginPassword))! {
+            
+            return false
+        }
+        return true
+    }
+    
+    // keyboard가 보여질 때 어떤 동작을 수행
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else { return }
+        
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardHeight: CGFloat // 키보드의 높이
+        
+        if #available(iOS 11.0, *) {
+            keyboardHeight = keyboardFrame.cgRectValue.height - self.view.safeAreaInsets.bottom
+        } else {
+            keyboardHeight = keyboardFrame.cgRectValue.height
+        }
+        
+        // animation 함수
+        // 최종 결과물 보여줄 상태만 선언해주면 애니메이션은 알아서
+        // duration은 간격
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            
+            self.logoImgView.alpha = 0
+            
+            // +로 갈수록 y값이 내려가고 -로 갈수록 y값이 올라간다.
+            self.stackViewCenterY.constant = -keyboardHeight/2 + 50
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    // keyboard가 사라질 때 어떤 동작을 수행
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            
+            // 원래대로 돌아가도록
+            self.logoImgView.alpha = 1.0
+            self.stackViewCenterY.constant = 0
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    
+    // observer
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+ */
