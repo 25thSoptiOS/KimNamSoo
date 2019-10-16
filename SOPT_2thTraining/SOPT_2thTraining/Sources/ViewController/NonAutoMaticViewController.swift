a80e207d7138a24d84d6ac2cf7482aab06c4f725
namsoo5 <are34@naver.com>
2019-10-16 17:56:28 +0900

    autoLayout Programmatic 키보드 내리기

--- SOPT_2thTraining/SOPT_2thTraining/Sources/ViewController/NonAutoMaticViewController.swift
+++ SOPT_2thTraining/SOPT_2thTraining/Sources/ViewController/NonAutoMaticViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NonAutoMaticViewController: UIViewController {
+class NonAutoMaticViewController: UIViewController, UITextFieldDelegate{
 
     var logoImg: UIImageView!
     var backImg: UIImageView!
@@ -18,8 +18,11 @@ class NonAutoMaticViewController: UIViewController {
     var pwTextField: UITextField!
     var loginBt: UIButton!
     var pwLine: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerNotification()
         
         logoImageGenerate()
         backgroundImageGenerate()
@@ -32,11 +35,57 @@ class NonAutoMaticViewController: UIViewController {
         loginButtonGenerate()
         signUpButtonGenerate()
         
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        unregisterNotification()
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    func registerNotification(){
+        NotificationCenter.default.addObserver(self, selector: #selector(moveUp), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+         NotificationCenter.default.addObserver(self, selector: #selector(moveDown), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    func unregisterNotification(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc func moveUp(_ notification: NSNotification){
+        
+        guard let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height else { return }
+        
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        UIView.animate(withDuration: duration, delay: 0 , animations: {
+            
+            self.logoImg.alpha = 0.0
+            // layout 줄이기
+            self.idLabel.
+            
+        })
+        
+    }
+    
+    @objc func moveDown(_ notification: NSNotification){
+        
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        UIView.animate(withDuration: duration, delay: 0,
+                       animations: {
+                        
+                        self.logoImg.alpha = 1.0
+                        
+            
+        })
     }
 }
 
