254d9321f20ff874091802e7bd84524c37ff6dc5
namsoo5 <are34@naver.com>
2019-10-26 17:30:29 +0900

    pageCtrl 로직

--- SOPT_2차 실습/SOPT_2thTraining/Sources/ViewController/BannerViewController.swift
+++ SOPT_2차 실습/SOPT_2thTraining/Sources/ViewController/BannerViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import CHIPageControl
 
 class BannerViewController: UIViewController {
-
+    
     @IBOutlet weak var bannerCV: UICollectionView!
+    @IBOutlet weak var pageControl: CHIPageControlAleppo!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setBanner()
         
         bannerCV.delegate = self
@@ -31,39 +33,51 @@ extension BannerViewController{
         
         appDelegate.bannerList = [banner1, banner2, banner3]
     }
+    
+    
 }
 
 
 extension BannerViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     
-     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    //        if collectionView == self.workCV {
-    //            return appDelegate.workList.count
-    //        }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //        if collectionView == self.workCV {
+        //            return appDelegate.workList.count
+        //        }
         
         return appDelegate.bannerList.count
-        }
+    }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    //        if collectionView == self.workCV {
-    //            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WorkCell", for: indexPath) as! WorkCell
-    //
-    //            let work = appDelegate.workList[indexPath.row]
-    //
-    //            cell.workIdx = work.workIdx
-    //            cell.workImg.image = work.workMainImg
-    //
-    //            return cell
-    //        }
-    //        else {
-                let bannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
-                
-                let banner = appDelegate.bannerList[indexPath.row]
-                bannerCell.image?.image = banner.bannerImg
-                
-                // bannerCell.banner = bannerList.
-                return bannerCell
-    //        }
-//        }
+        //        if collectionView == self.workCV {
+        //            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WorkCell", for: indexPath) as! WorkCell
+        //
+        //            let work = appDelegate.workList[indexPath.row]
+        //
+        //            cell.workIdx = work.workIdx
+        //            cell.workImg.image = work.workMainImg
+        //
+        //            return cell
+        //        }
+        //        else {
+        let bannerCell: BannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
+        
+        let banner = appDelegate.bannerList[indexPath.row]
+        bannerCell.image?.image = banner.bannerImg
+        
+        // bannerCell.banner = bannerList.
+        return bannerCell
+        //        }
+        //        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let x = scrollView.contentOffset.x
+        let w = scrollView.bounds.size.width
+        let currentPage = Int(ceil(x/w))
+        print(currentPage)
+        self.pageControl.set(progress: currentPage, animated: true)
+        
     }
 }
