62775035de3978cca0769f9e33dcc9b85095de52
namsoo5 <are34@naver.com>
2019-11-18 10:02:17 +0900

    상단 내정보레이아웃 구현

---
+++ SOPT_5th/SOPT_5th/Sources/ViewController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SOPT_5th
+//
+//  Created by 남수김 on 2019/11/16.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
